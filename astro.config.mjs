bfb3f32c6a0511b2d5793696ea5b504a494e7181
Al-Hadee Mohd Roslan <kodeexii@yahoo.com>
2026-03-08 11:23:19 +0800

    chore: install and configure Cloudflare adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 import markdoc from '@astrojs/markdoc';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
+import cloudflare from '@astrojs/cloudflare';
 
+// https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'server', // Membolehkan mod Hybrid/SSR
+  adapter: cloudflare(), // Menggunakan adapter Cloudflare untuk Pages
   integrations: [
     markdoc(),
     react(),
