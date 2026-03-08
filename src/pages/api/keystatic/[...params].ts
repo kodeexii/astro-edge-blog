2dbc8ed345b9b49d0120f95d91998b46f6e161d8
Al-Hadee Mohd Roslan <kodeexii@yahoo.com>
2026-03-09 00:17:56 +0800

    fix: resolve correct relative path for keystatic.config in manual setup

--- src/pages/api/keystatic/[...params].ts
+++ src/pages/api/keystatic/[...params].ts
@@ -1,4 +1,4 @@
 import { makeApi } from '@keystatic/astro/api';
-import config from '../../../keystatic.config';
+import config from '../../../../keystatic.config';
 
 export const all = makeApi(config);
